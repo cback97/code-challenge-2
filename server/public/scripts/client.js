00e972a654ab7bcd64a105f8e75e7fd19d37b912
Christian Back <cdirt27@gmail.com>
2021-04-19 11:29:29 -0500

    on client side, created addJoke functiion to get values of input id's and on click, place them into an object to be console.log

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,8 +4,15 @@ $( document ).ready( onReady );
 
 function onReady() {
     console.log('DOM ready');
+    $('#addJokeButton').on('click', addJoke)
 }
 
 function addJoke() {
-    
+    let newJoke = {
+        whoseJoke: $('#whoseJokeIn').val(),
+        jokeQuestion: $('#questionIn').val(),
+        punchLine: $('#punchlineIn').val()
+    }
+    // test if newJoke is operable
+    console.log(newJoke);
 }
